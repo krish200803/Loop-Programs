c630ec440e6d424298d3610e7e30337234b77fc8
krish200803 <145696031+krish200803@users.noreply.github.com>
2025-02-07 11:48:45 +0530

    Add files via upload

---
+++ Number100.java
@@ -0,0 +1,16 @@
+//import java.util.Scanner;
+class  Even100
+{
+	public static void main(String[] args) 
+	{
+		//Scanner sc = new Scanner(System.in);
+		//System.out.println("Enter the Number");
+		for(int i=1; i<=100; i++)
+		{
+			if(i%2 == 0)
+			{
+				System.out.print(i+" ");
+			}
+		}
+	}
+}
